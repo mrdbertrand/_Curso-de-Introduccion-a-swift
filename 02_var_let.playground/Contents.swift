30fabed60e067dea4698852563ba98c329cb4ea6
mrdbertrand <mrdbertrand>
2022-02-01 19:48:33 -0600

    variables and constants

---
+++ 02_var_let.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+//Constante let
+let maximumNumberOfLoginAttempts = 3
+
+//Vriable let
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage : String
+
+welcomeMessage = "Hola, ¿Que tal?"
+
+var red, green, blue : Double
+
+//Alt + p
+let π = 3.1416
+
+//Cmd + ctrl + space bar
+let 👽 = "alien"
+print(👽)
+
+//Imprimir mas de una variable
+print("El numero de login actual es: \(currentLoginAttempt) de un total disponible de: \(maximumNumberOfLoginAttempts)")
+
+/* Esto es un comentario
+  Ocupavarias líneas
+  Y puedo escribir muchas cosas */
+
+/* Este es un comentario multilinea
+ /* Esto es un comentario anidado*/
+ ste es un comentario multilinea */
+
